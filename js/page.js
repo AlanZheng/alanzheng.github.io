1b9f846da7c8fb14fe5b9e9f6f5b69a08eca6170
AlanZheng <machinecat1666@gmail.com>
2018-02-25 13:23:10 +0800

    update

--- js/page.js
+++ js/page.js
@@ -6,26 +6,29 @@ function logobtn()
         s[i].classList.toggle('active')
     }
 }
-
+/*
 function createSidebar()
 {
     var node = document.getElementsByClassName("sidebar");
     if( node.length > 0 )
-        node[0].innerHTML('
-        <ul>
-              <li>Tutorials
-                <ul class="submenu">
-                    <li><a href="tutorials/css">CSS</a></li>
-                    <li><a href="tutorials/gimp">GIMP</a></li>
-                </ul>
-              </li>
-              <li>Projects
-                <ul class="submenu">
-                    <li><a href="projects/gimp">GIMP</a></li>
-                    <li><a href="projects/steemtools">SteemitTools</a></li>
-                </ul>
-              </li>
-              <li>Demos</li>
-        </ul>'
-        );
+    {
+        var sb = '
+        <ul> \
+              <li>Tutorials \
+                <ul class="submenu"> \
+                    <li><a href="tutorials/css">CSS</a></li> \
+                    <li><a href="tutorials/gimp">GIMP</a></li> \
+                </ul> \
+              </li> \
+              <li>Projects \
+                <ul class="submenu"> \
+                    <li><a href="projects/gimp">GIMP</a></li> \
+                    <li><a href="projects/steemtools">SteemitTools</a></li> \
+                </ul> \
+              </li> \
+              <li>Demos</li> \
+        </ul>';
+        node[0].innerHTML();
+    }
 }
+*/
