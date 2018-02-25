2eed58d8e32d052a90b6d5f9e0980274b3107f2e
AlanZheng <machinecat1666@gmail.com>
2018-02-25 13:12:46 +0800

    update

--- js/page.js
+++ js/page.js
@@ -6,3 +6,26 @@ function logobtn()
         s[i].classList.toggle('active')
     }
 }
+
+function createSidebar()
+{
+    var node = document.getElementsByClassName("sidebar");
+    if( node.length > 0 )
+        node[0].innerHTML('
+        <ul>
+              <li>Tutorials
+                <ul class="submenu">
+                    <li><a href="tutorials/css">CSS</a></li>
+                    <li><a href="tutorials/gimp">GIMP</a></li>
+                </ul>
+              </li>
+              <li>Projects
+                <ul class="submenu">
+                    <li><a href="projects/gimp">GIMP</a></li>
+                    <li><a href="projects/steemtools">SteemitTools</a></li>
+                </ul>
+              </li>
+              <li>Demos</li>
+        </ul>'
+        );
+}
