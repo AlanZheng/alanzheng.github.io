d5bfa6c0fc0f5afee5893c52858f205905f6e6ce
AlanZheng <machinecat1666@gmail.com>
2018-03-01 17:55:23 +0800

    update

--- demo/js/config.js
+++ demo/js/config.js
@@ -73,7 +73,7 @@ $(function() {
 	simpleStore.init({
 
 		// brand can be text or image URL
-		brand : "My Design",
+		brand : "Products",
 
 		// numder of products per row (accepts 1, 2 or 3)
 		numColumns : 3,
