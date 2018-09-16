ad499ff7a2f9fcc784d958218323e71e3ba6f6fb
AlanZheng <machinecat1666@gmail.com>
2018-09-16 16:38:48 +0800

    update

---
+++ js/cms_tutorials_python_config.js
@@ -0,0 +1,63 @@
+$(function() {
+
+	CMS.init({
+
+		// Name of your site or location of logo file ,relative to root directory (img/logo.png)
+		siteName: '',
+
+		// Tagline for your site
+		siteTagline: '',
+
+		// Email address
+		siteEmail: 'microsuncn@gmail.com',
+
+		// Name
+		siteAuthor: 'AlanZheng',
+
+		// Navigation items
+		siteNavItems: [
+			//{ name: 'Github', href: 'https://github.com/alanzheng', newWindow: false},
+			//{ name: 'About'}
+		],
+
+		// Posts folder name
+		postsFolder: 'tutorials/python',
+
+		// Homepage posts snippet length
+		postSnippetLength: 120,
+
+		// Pages folder name
+		pagesFolder: 'tutorials/python',
+
+		// Site fade speed
+		fadeSpeed: 300,
+
+		// Site footer text
+		footerText: '&copy; ' + new Date().getFullYear() + ' All Rights Reserved.',
+
+		// Mode 'Github' for Github Pages, 'Apache' for Apache server. Defaults
+		// to Github
+		mode: 'Github',
+
+		// If Github mode is set, your Github username and repo name. Defaults
+		// to Github pages branch (gh-pages)
+		githubUserSettings: {
+			username: 'alanzheng',
+			repo: 'alanzheng.github.io'
+		}
+
+	});
+
+	// Markdown settings
+	marked.setOptions({
+		renderer: new marked.Renderer(),
+		gfm: true,
+		tables: true,
+		breaks: false,
+		pedantic: false,
+		sanitize: true,
+		smartLists: true,
+		smartypants: false
+	});
+
+});
